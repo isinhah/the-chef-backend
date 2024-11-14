02ff0af291969cdf467166d93be6462a020ad758
isinhah <contato.isabelhenrique@gmail.com>
2024-11-14 13:33:43 -0300

    feat: anotação de validação dos dados de request de restaurante e categoria

--- src/main/java/com/api/the_chef_backend/model/dtos/request/RestaurantRequestDTO.java
+++ src/main/java/com/api/the_chef_backend/model/dtos/request/RestaurantRequestDTO.java
@@ -1,13 +1,25 @@
 package com.api.the_chef_backend.model.dtos.request;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record RestaurantRequestDTO(
+        @NotBlank(message = "O nome é obrigatório.")
+        @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres.")
         String name,
+        @NotBlank(message = "CPF ou CNPJ é obrigatório.")
+        @Size(max = 100, message = "CPF/CNPJ deve ter no máximo 14 caracteres.")
         String cpfOrCnpj,
+        @NotBlank(message = "O telefone é obrigatório.")
+        @Size(max = 15, message = "O telefone deve ter no máximo 15 caracteres.")
         String phone,
+        @Positive(message = "A quantidade de mesas deve ser um número positivo.")
         int tableQuantity,
+        @DecimalMin(value = "0.0", message = "A comissão do garçom não pode ser negativa.")
         BigDecimal waiterCommission
 ) {
 }
